94e24503aac26b5801e3d68ea51997920a45457d
Gyamjo Dong <nischallama222@gmail.com>
2024-05-02 13:55:00 +0545

    button test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Home</div>;
+  return (
+    <div className="App">
+      Home
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
 }
 
 export default App;
